5ca663acc53a4bb64f0a0b40e5248a2d23d1c01c
Robert Le <robertle982@gmail.com>
2024-07-07 18:18:27 -0400

    added agents ice braker

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,43 +1,46 @@
+import os
 
-from dotenv import load_dotenv
-load_dotenv()
-
-
-from langchain.prompts.prompt import PromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
-from langchain_core.tools import Tool
-
-from langchain.agents import(
+from langchain import hub
+from langchain.agents import (
     create_react_agent,
     AgentExecutor,
 )
+from langchain_core.tools import Tool
+from langchain_openai import ChatOpenAI
+from langchain.prompts.prompt import PromptTemplate
+from dotenv import load_dotenv
+from tools.tools import get_profile_url_tavily
 
-from langchain import hub
+load_dotenv()
 
-# def lookup(name: str) -> str:
-#     return "https://www.linkedin.com/in/eden-marco/"
 
 def lookup(name: str) -> str:
     llm = ChatOpenAI(
         temperature=0,
         model_name="gpt-3.5-turbo",
+        openai_api_key=os.environ["OPENAI_API_KEY"],
     )
-    template = """given the full name{name_of_person} I want you to get it me a link to their Linkedin profile page. Your answer should only contain a URL."""
-    
+    template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
+                          Your answer should contain only a URL"""
+
     prompt_template = PromptTemplate(
-        template=template,input_variables=["name_of_person"]
+        template=template, input_variables=["name_of_person"]
     )
     tools_for_agent = [
         Tool(
             name="Crawl Google 4 linkedin profile page",
-            func="?"
-            description="useful for when you need get the LinkedIn Page URL"
+            func=get_profile_url_tavily,
+            description="useful for when you need get the Linkedin Page URL",
         )
     ]
-    # langchain object
+
     react_prompt = hub.pull("hwchase17/react")
-    
-    
-    if __name__ == "__main__":
-        linkedin_url = lookup(name="Eden Marco")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linked_profile_url = result["output"]
+    return linked_profile_url
--- ice_breaker.py
+++ ice_breaker.py
@@ -3,21 +3,16 @@ from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
-# Load environment variables from the .env file
-load_dotenv()
+from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-    
-    
-    
-    
-def scrape_linkedin_profile(linkedin_profile_url):
-    # This function should scrape LinkedIn profile data
-    # For now, let's return dummy data
-    return "LinkedIn profile information about Eden Marco"
 
-if __name__ == '__main__':
-    print("Hello LangChain")
+
+def ice_break_with(name: str) ->str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
     
+     
     summary_template = """
     Given LinkedIn info {information} about a person, I want you to create:
     1. A short summary
@@ -30,9 +25,23 @@ if __name__ == '__main__':
     
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     
+        
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    linkedin_data = scrape_linkedin_profile( linkedin_profile_url="https://www.linkedin.com/in/eden-marco")
+
     
     res = chain.invoke(input={"information": linkedin_data})
     
     print(res)
+    
+    
+# def scrape_linkedin_profile(linkedin_profile_url):
+#     # This function should scrape LinkedIn profile data
+#     # For now, let's return dummy data
+#     return "LinkedIn profile information about Eden Marco"
+
+if __name__ == '__main__':
+    load_dotenv()
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Marco")
+   
+
