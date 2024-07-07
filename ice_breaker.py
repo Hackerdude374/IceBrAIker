1fce5356143bc12d96ffd162de8f9ff88dbd3ecf
Robert Le <robertle982@gmail.com>
2024-07-07 18:41:38 -0400

    installed pckgs, made frontend

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+#from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from chains.custom_chains import (
     get_summary_chain,
     get_interests_chain,
@@ -20,27 +20,30 @@ def ice_break_with(name: str) -> Tuple[Summary, TopicOfInterest, IceBreaker, str
     # Step 2: Get information from their LinkedIn profile
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
-    # Step 3: Find their Twitter profile
-    twitter_username = twitter_lookup_agent(name=name)
-    # Step 4: Get their recent tweets
-    tweets = scrape_user_tweets(username=twitter_username)
+    # # Step 3: Find their Twitter profile
+    # twitter_username = twitter_lookup_agent(name=name)
+    # # Step 4: Get their recent tweets
+    # tweets = scrape_user_tweets(username=twitter_username)
 
     # Step 5: Summarize the LinkedIn and Twitter information
     summary_chain = get_summary_chain()
     summary_and_facts: Summary = summary_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets},
+       # input={"information": linkedin_data, "twitter_posts": tweets},
+        input={"information": linkedin_data},
     )
 
     # Step 6: Find topics they might be interested in
     interests_chain = get_interests_chain()
     interests: TopicOfInterest = interests_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
+       # input={"information": linkedin_data, "twitter_posts": tweets}
+        input={"information": linkedin_data},
     )
 
     # Step 7: Create ice breakers using their LinkedIn and Twitter information
     ice_breaker_chain = get_ice_breaker_chain()
     ice_breakers: IceBreaker = ice_breaker_chain.invoke(
-        input={"information": linkedin_data, "twitter_posts": tweets}
+       # input={"information": linkedin_data, "twitter_posts": tweets}
+           input={"information": linkedin_data},
     )
 
     # Return the summary, interests, ice breakers, and profile picture URL
